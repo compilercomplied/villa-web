a08ca789ec662c660109688cb262745dc0086287
gdario <git.gdario@gmail.com>
2021-03-30 09:55:47 +0200

    fix(app): move IDs to env; remove unused code

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,16 @@
 import { Auth0Provider } from '@auth0/auth0-react';
-import React from 'react';
 import './App.css';
 import { ToastBucket } from './components/notification/toast';
 import { ToastProvider } from './contexts/toast';
 import AppRouter from './routing/app-router';
 
+
 function App() {
   return (
     <Auth0Provider
-      domain="gdariodev.eu.auth0.com"
-      clientId="7smC2h3PphTDfo0G24nQPjfVa9m7S4mU"
-      audience="pfm.gdario.dev"
+      domain={ process.env.REACT_APP_AUTH0_DOMAIN           ?? "" }
+      clientId={ process.env.REACT_APP_AUTH0_VILLA_ID       ?? "" }
+      audience={ process.env.REACT_APP_AUTH0_VILLA_AUDIENCE ?? "" }
       useRefreshTokens={ true }
       redirectUri={ window.location.origin }
     >
